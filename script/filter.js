747a873145ce9f2439009b80546205364fa54efa
runsincircles <159605281+runsincircles@users.noreply.github.com>
2024-03-05 21:02:06 -0600

    Update filter.js

--- script/filter.js
+++ script/filter.js
@@ -4,7 +4,7 @@ module.exports.config = {
 		role: 2,
 	credits: "Marjhun Baylon and Miko Mempin",
 		description: "Filter Faceboook User",
-		hasPrefix: false,
+		hasPrefix: true,
 		usages: "",
 		cooldowns: 5
 };
